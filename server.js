818bfe53357726bc840b9f83155a7c56af42ce5e
Shakira Reid - Thomas <kaethedev@gmail.com>
2019-10-04 05:06:14 -0400

    updated .gitignore file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
 
 const app = express();
 
@@ -19,6 +21,12 @@ const db = require("./config/keys").mongoURI;
 mongoose.connect(db, {useNewUrlParser: true})
 .then(() => console.log("MongoDB successfully connected!"))
 .catch(err => console.log(err));
+
+//Passport Config
+require("./config/passport")(passport);
+
+//ROUTES
+app.use("/api/users", users);
 
 //process.env.port == HEROKU PORT
 
